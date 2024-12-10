da912880df4e562e1ff11abbb5900951d173ab96
Danirolan21 <danirolan21@hotmail.com>
2024-12-10 12:51:19 +0100

    Add project files.

---
+++ FundamentosNetCore/Form1.cs
@@ -0,0 +1,19 @@
+namespace FundamentosNetCore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Fundamentos .NET Core";
+            this.SuspendLayout();
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            this.txtNombre.Location = new Point(104, 67);
+            this.txtNombre.Text = "Hola Mundo";
+            this.txtNombre.Width = 125;
+        }
+    }
+}
